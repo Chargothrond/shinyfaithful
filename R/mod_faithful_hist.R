94f5476ef2d48b6e67f318d78030551fde15e5fa
Peter Schmid <peter.schmid@mirai-solutions.com>
2023-06-20 12:45:30 +0200

    Copy over proper mod_faithful_hist_ui

--- R/mod_faithful_hist.R
+++ R/mod_faithful_hist.R
@@ -7,10 +7,19 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
-mod_faithful_hist_ui <- function(id){
+mod_faithful_hist_ui <- function(id) {
   ns <- NS(id)
   tagList(
-
+    fluidRow(
+      column(3,
+             sliderInput(ns("bins"), "Number of bins:", min = 1, max = 50, value = 30),
+             checkboxInput(ns("density"), label = "density")
+      ),
+      # Show a plot of the generated distribution
+      column(9,
+             plotOutput(ns("distPlot"))
+      )
+    )
   )
 }
 
@@ -24,6 +33,7 @@ mod_faithful_hist_server <- function(id){
     variable <- "waiting"
     x    <- datasets::faithful[, variable, drop = FALSE]
     bins <- reactive(seq(min(x), max(x), length.out = input$bins + 1))
+
     output$distPlot <- renderPlot({
       # draw the histogram with the specified number of bins
       plot_hist(x = x, breaks = bins(), freq = !input$density)
